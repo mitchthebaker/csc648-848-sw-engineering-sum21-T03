2b800ed3d8665e0d5586402fd0d82917339ea627
kennethchuson2 <kchuson@mail.sfsu.edu>
2021-06-07 19:06:32 -0700

    I added myself in the about page

--- application/src/App.js
+++ application/src/App.js
@@ -1,6 +1,7 @@
 import Home from './pages/Home';
 import About from './pages/About';
 import Baker from './components/Baker';
+import Chuson from './components/Chuson'
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" component={Home} exact />
         <Route path="/about" component={About} />
         <Route path="/mbaker" component={Baker} />
+        <Route path="/kenneth" component={Chuson} />
       </Switch>
     </main>
   );
---
+++ application/src/components/Chuson.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Chuson = () => {
+    return (
+        <div className="team-member">
+            <h2> Kenneth Chuson </h2>
+            <img
+                src={`/uploads/chuson.jpg`}
+                className="team-member-image"
+                alt="Failed to load."
+            />
+            <p> Hi my name is Kenneth, I am interested in competitive programming and doing graphics design such as animation and abstract art.</p>
+        </div>
+    );
+};
+
+export default Chuson;
