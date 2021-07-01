6d0c8290ecc62c46a0b3461caad6ebbe435bbdc4
kennethchuson2 <kchuson@mail.sfsu.edu>
2021-07-01 00:27:40 -0700

    added styling for login and sign up

--- application/client/src/pages/Login.js
+++ application/client/src/pages/Login.js
@@ -7,19 +7,22 @@ const Login = () => {
             <NavLink className="nav-link" to="/"> Home </NavLink>
             <NavLink className="nav-link" to="/about"> About </NavLink>
             <NavLink className="nav-link" to="/profile"> Profile </NavLink>
-            <h1>Login / Sign up</h1>  
+            <h1 className="Login-Title">Login / Sign up</h1>  
 
-            <form>
-                <label>
-                    <p>UserName</p>
-                    <input type="text" name="userName-Login" required/>
-                    <p>Password</p>
-                    <input type="text" name="userName-Password" required/>
-                </label>
-                <input type="submit" value="Submit" name="Submit-Login"/>
-            </form>
-            
-            <NavLink className="nav-link" to="/sign_up"> Sign Up </NavLink> 
+            <div className="container-login">
+                <form>
+                    <label className="label-login">
+                        <p>UserName</p>
+                        <input type="text" name="userName-Login" required/>
+                        <p>Password</p>
+                        <input type="text" name="userName-Password" required/>
+                        <input className="Submit-Login" type="submit" value="Submit" name="Submit-Login"/>
+                    </label>
+                </form>
+                <div className="Sign-Up-LoginContainer">
+                    <NavLink className="nav-link" to="/sign_up"> Sign Up </NavLink> 
+                </div>
+            </div>
         </div>
     );
 };
