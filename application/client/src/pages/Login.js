b2471e7fac08845c8533892bba1908e6643ad07e
Krina1103 <kpatel107@horizon.csueastbay.edu>
2021-07-01 08:10:10 -0700

    Added buttons to Login Page

--- application/client/src/pages/Login.js
+++ application/client/src/pages/Login.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Login = () => {
+// const Login = () => {
+
+    class Login extends React.Component{
+        state={
+            Login:'',
+            Password:''
+        }
+    
+        handleChange = (e) =>{
+            const {name,value} = e.target
+            this.setState({[name]:value})
+        }
+    
+        handleSubmit = (e) =>{
+            e.preventDefault()
+            this.props.isLogin(true)
+        }
+    render() {
     return (
         <div className="settings-wrapper">
             <NavLink className="nav-link" to="/"> Home </NavLink>
@@ -13,18 +31,23 @@ const Login = () => {
                 <form>
                     <label className="label-login">
                         <p>UserName</p>
-                        <input type="text" name="userName-Login" required/>
+                        <input type="text" name="Login" placeholder="email..." required onChange={this.handleChange} />
                         <p>Password</p>
-                        <input type="text" name="userName-Password" required/>
-                        <input className="Submit-Login" type="submit" value="Submit" name="Submit-Login"/>
+                        <input type="text" name="Password" placeholder="password..." required onChange={this.handleChange}/>
+                        {/* <input className="Submit-Login" type="submit" value="Submit" name="Submit-Login"/> */}
+                       <p> <button onSubmit={this.handleSubmit}>Log In</button> </p>
                     </label>
                 </form>
-                <div className="Sign-Up-LoginContainer">
-                    <NavLink className="nav-link" to="/sign_up"> Sign Up </NavLink> 
                 </div>
-            </div>
+                <div className="Sign-Up-LoginContainer">
+                    <p>Don't Have an Account? Sign Up Now!  
+                    <NavLink className="nav-link" to="/sign_up"> <button onSubmit={this.handleSubmit}>  Sign Up </button> </NavLink> 
+                   </p>
+                </div>
+            
         </div>
     );
-};
+    }
+}
 
 export default Login;
