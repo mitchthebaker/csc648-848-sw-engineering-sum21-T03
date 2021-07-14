ee812bb850adfe5489ef18284016b9a82b563b70
kennethchuson2 <kchuson@mail.sfsu.edu>
2021-07-13 20:11:35 -0700

    added a chart for an activities component for seller settings

--- application/client/src/components/SellersSettingComponents/Activities.js
+++ application/client/src/components/SellersSettingComponents/Activities.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect, useDispatch, useSelector } from 'react-redux';
+import { Bar } from 'react-chartjs-2'; 
 
 
 const sellerActivitiesSettings = () => {
@@ -8,7 +8,21 @@ const sellerActivitiesSettings = () => {
    return (
     <div className="sellerActivitiesSettings-wrapper">
         <div className="sellerActivitiesSettings-firstContainer">
-            
+            <Bar data={{
+                labels: ['Sells', 'Posts', 'Active', 'Inactive'], 
+                datasets: [ 
+                        {
+                            label: '# of activities', 
+                            data: [12,20,5,8,18,11,21]
+                        },
+                    ],
+                }}
+                height={400}
+                width={600} 
+                options={{
+                    maintainAspectRatio: false,
+                }}
+            />
         </div>
     </div>
    );
