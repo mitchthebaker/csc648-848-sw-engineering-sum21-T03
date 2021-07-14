264683f1e7f9b3770bfeb6f7138db20522a8b14e
kennethchuson2 <kchuson@mail.sfsu.edu>
2021-07-14 01:31:47 -0700

    fixed overflow account component settings

--- application/client/src/components/BuyersSettingComponents/Account.js
+++ application/client/src/components/BuyersSettingComponents/Account.js
@@ -8,31 +8,39 @@ const buyerAccountSettings = () => {
    return (
     <div className="buyerAccountSettings-wrapper">
         <div className="buyerAccountSettings-firstContainer">
-            <p>FirstName</p>
-            <input type="text"/>
-            <p>LastName</p>
-            <input type="text"/>
-            <p>Birthday</p>
-            <input type="date"/>
-            <p>Email</p>
-            <input type="text"/>
-            <p>Phone</p>
-            <input type="text"/>
-            <p>UserName</p>
-            <input type="text"/>
-            <p>Password</p>
-            <input type="text"/>
+            <div className="buyerAccountSettings-firstContainer_oneSide">
+                <p>FirstName</p>
+                <input type="text"/>
+                <p>LastName</p>
+                <input type="text"/>
+                <p>Birthday</p>
+                <input type="date"/>
+                <p>Email</p>
+            </div>
+            <div className="buyerAccountSettings-firstContainer_twoSide">
+                <input type="text"/>
+                <p>Phone</p>
+                <input type="text"/>
+                <p>UserName</p>
+                <input type="text"/>
+                <p>Password</p>
+                <input type="text"/>
+            </div>
         </div>
         <div className="buyerAccountSettings-secondContainer">
             <h3>Credit Debit Card</h3>
-            <p>Card Number</p>
-            <input type="text"/>
-            <p>Expiration Date</p>
-            <input type="text"/>
-            <p>CVV</p>
-            <input type="text"/>
-            <p>Postal Code</p>
-            <input type="text"/>
+            <div className="buyerAccountSettings-secondContainer_oneSide">
+                <p>Card Number</p>
+                <input type="text"/>
+                <p>Expiration Date</p>
+                <input type="text"/>
+            </div>
+            <div className="buyerAccountSettings-secondContainer_twoSide">
+                <p>CVV</p>
+                <input type="text"/>
+                <p>Postal Code</p>
+                <input type="text"/>
+            </div>
         </div>
     </div>
    );
--- application/client/src/components/SellersSettingComponents/Account.js
+++ application/client/src/components/SellersSettingComponents/Account.js
@@ -8,31 +8,39 @@ const sellerAccountSettings = () => {
    return (
     <div className="sellerAccountSettings-wrapper">
         <div className="sellerAccountSettings-firstContainer">
-            <p>FirstName</p>
-            <input type="text"/>
-            <p>LastName</p>
-            <input type="text"/>
-            <p>Birthday</p>
-            <input type="date"/>
-            <p>Email</p>
-            <input type="text"/>
-            <p>Phone</p>
-            <input type="text"/>
-            <p>UserName</p>
-            <input type="text"/>
-            <p>Password</p>
-            <input type="text"/>
+            <div className="sellerAccountSettings-firstContainer_oneSide">
+                <p>FirstName</p>
+                <input type="text"/>
+                <p>LastName</p>
+                <input type="text"/>
+                <p>Birthday</p>
+                <input type="date"/>
+                <p>Email</p>
+            </div>
+            <div className="sellerAccountSettings-firstContainer_twoSide">
+                <input type="text"/>
+                <p>Phone</p>
+                <input type="text"/>
+                <p>UserName</p>
+                <input type="text"/>
+                <p>Password</p>
+                <input type="text"/>
+            </div>
         </div>
         <div className="sellerAccountSettings-secondContainer">
             <h3>Credit Debit Card</h3>
-            <p>Card Number</p>
-            <input type="text"/>
-            <p>Expiration Date</p>
-            <input type="text"/>
-            <p>CVV</p>
-            <input type="text"/>
-            <p>Postal Code</p>
-            <input type="text"/>
+            <div className="sellerAccountSettings-secondContainer_oneSide">
+                <p>Card Number</p>
+                <input type="text"/>
+                <p>Expiration Date</p>
+                <input type="text"/>
+            </div>
+            <div className="sellerAccountSettings-secondContainer">
+                <p>CVV</p>
+                <input type="text"/>
+                <p>Postal Code</p>
+                <input type="text"/>
+            </div>
         </div>
     </div>
    );
