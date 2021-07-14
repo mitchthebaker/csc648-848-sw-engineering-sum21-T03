f0c306996fd2939e1dfb0ee1414fafe3dbbee02f
Mitchel Baker <mitchrpbaker@gmail.com>
2021-07-13 18:45:18 -0700

    Fixed display of components for BuyerSettings.js

--- application/client/src/pages/BuyersSettings.js
+++ application/client/src/pages/BuyersSettings.js
@@ -2,10 +2,10 @@ import React, {useEffect, useState} from 'react';
 import Logo from '../components/Modules/Logo';
 import NavBar from '../components/Modules/NavBar';
 import ProductCreationForm from '../components/ProductCreationForm';
-import AccountBuyers from '../components/BuyersSettingComponents/Account'; 
-import ActivitiesBuyers from '../components/BuyersSettingComponents/Activities';
-import ProfileBuyers from '../components/BuyersSettingComponents/Profile';
-import ShippingBuyers from '../components/BuyersSettingComponents/Shipping';
+import Account from '../components/BuyersSettingComponents/Account'; 
+import Activities from '../components/BuyersSettingComponents/Activities';
+import Profile from '../components/BuyersSettingComponents/Profile';
+import Shipping from '../components/BuyersSettingComponents/Shipping';
 
 
 import { connect, useDispatch } from 'react-redux';
@@ -15,30 +15,42 @@ const BuyerSettings = (props) => {
 
     // test comment for showing multiple commits in a pull request 
 
-    const [toggleAccount, set_toggleAccount] = useState([false]); 
-    const [toggleProfile, set_toggleProfile] = useState([false]); 
-    const [toggleProducts, set_toggleProducts] = useState([false]); 
-    const [toggleShipping, set_toggleShipping] = useState([false]); 
-    const [toggleActivities, set_toggleActivities] = useState([false]); 
+    const [toggleAccount, set_toggleAccount] = useState(false); 
+    const [toggleProfile, set_toggleProfile] = useState(false); 
+    const [toggleProducts, set_toggleProducts] = useState(false); 
+    const [toggleShipping, set_toggleShipping] = useState(false); 
+    const [toggleActivities, set_toggleActivities] = useState(false); 
 
     const toggleAccount_func = () => {
       set_toggleAccount(!toggleAccount); 
+
+      console.log(toggleAccount);
+      console.log(toggleProfile);
+      console.log(toggleProducts);
+      console.log(toggleShipping);
+      console.log(toggleActivities);
+
       if (toggleProfile) {
+        console.log('profile: ' + toggleProfile);
         set_toggleProfile(!toggleProfile); 
       }
       if (toggleProducts) {
+        console.log('products: ' + toggleProducts);
         set_toggleProducts(!toggleProducts); 
       }
       if (toggleShipping) {
+        console.log('shipping: ' + toggleShipping);
         set_toggleShipping(!toggleShipping); 
       }
       if (toggleActivities) {
+        console.log('activities: ' + toggleActivities);
         set_toggleActivities(!toggleActivities); 
       }
     }
 
     const toggleProfile_func = () => {
       set_toggleProfile(!toggleProfile); 
+
       if (toggleAccount) {
         set_toggleAccount(!toggleAccount); 
       }
@@ -55,11 +67,12 @@ const BuyerSettings = (props) => {
 
     const toggleProducts_func = () => {
       set_toggleProducts(!toggleProducts); 
+
       if (toggleAccount) {
         set_toggleAccount(!toggleAccount); 
       }
-      if (toggleProducts) {
-        set_toggleProducts(!toggleProducts); 
+      if (toggleProfile) {
+        set_toggleProfile(!toggleProfile); 
       }
       if (toggleShipping) {
         set_toggleShipping(!toggleShipping); 
@@ -74,11 +87,11 @@ const BuyerSettings = (props) => {
       if (toggleAccount) {
         set_toggleAccount(!toggleAccount); 
       }
+      if (toggleProfile) {
+        set_toggleProfile(!toggleProfile); 
+      }
       if (toggleProducts) {
         set_toggleProducts(!toggleProducts); 
-      }
-      if (toggleShipping) {
-        set_toggleShipping(!toggleShipping); 
       }
       if (toggleActivities) {
         set_toggleActivities(!toggleActivities); 
@@ -90,14 +103,14 @@ const BuyerSettings = (props) => {
       if (toggleAccount) {
         set_toggleProfile(!toggleAccount); 
       }
+      if (toggleProfile) {
+        set_toggleProfile(!toggleProfile); 
+      }
       if (toggleProducts) {
         set_toggleProducts(!toggleProducts); 
       }
       if (toggleShipping) {
         set_toggleShipping(!toggleShipping); 
-      }
-      if (toggleActivities) {
-        set_toggleActivities(!toggleActivities); 
       }
     }
 
@@ -108,18 +121,18 @@ const BuyerSettings = (props) => {
             <h1>Buyer Settings</h1>
             <div className="buyers-settings-wrapper">
               <ul className="buyers-selections">
+                <li onClick={toggleAccount_func}>Account</li>
                 <li onClick={toggleProfile_func}>Profile</li>
                 <li onClick={toggleProducts_func}>Products</li>
-                <li onClick={toggleAccount_func}>Account</li>
                 <li onClick={toggleShipping_func}>Shipping</li>
                 <li onClick={toggleActivities_func}>Activities</li>
               </ul>
               <div className="buyers-selection-area">
-                {toggleAccount == false? <AccountBuyers/> : null}
-                {toggleProfile == false? <ProfileBuyers/> : null}
-                {toggleProducts == false? <ProductCreationForm/> : null}
-                {toggleShipping == false? <ShippingBuyers/> : null}
-                {toggleActivities == false? <ActivitiesBuyers/> : null}
+                {toggleAccount !== false ? <Account/> : null}
+                {toggleProfile !== false ? <Profile/> : null}
+                {toggleProducts !== false ? <ProductCreationForm/> : null}
+                {toggleShipping !== false ? <Shipping/> : null}
+                {toggleActivities !== false ? <Activities/> : null}
               </div>
             </div>
             
