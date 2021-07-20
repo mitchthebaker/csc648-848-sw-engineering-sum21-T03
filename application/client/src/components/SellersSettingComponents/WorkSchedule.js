61d652711dadac0be1a5c280fea0d64f9fe96820
kennethchuson2 <kchuson@mail.sfsu.edu>
2021-07-20 00:57:01 -0700

    added style for the set up work on settings

--- application/client/src/components/SellersSettingComponents/WorkSchedule.js
+++ application/client/src/components/SellersSettingComponents/WorkSchedule.js
@@ -10,9 +10,10 @@ const SellerWorkScheduleSettings = () => {
   
    return (
     <div className="sellerWorkScheduleSettings-wrapper">
+        <h3>Schedule Set Up</h3>
         <div className="sellerWorkScheduleSettings-firstContainer">
-            <NavLink className="nav-link" to="/set-workSchedule"> Set your work schedule </NavLink>
-
+            <p>This will navigate to the other page when you set up your schedule. Remember, the more hours you work, the more money you have!</p>
+            <NavLink className="nav-link" to="/set-workSchedule"><button className="sellerSettingsButtons"> Set Up your Schedule </button></NavLink>
         </div>
     </div>
    );
